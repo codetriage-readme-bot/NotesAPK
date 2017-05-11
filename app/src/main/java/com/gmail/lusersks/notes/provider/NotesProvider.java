feea4aed686d9a6aefdb5f4ce0813b524a88f6cd
Raman Skaskevich <lusersks@gmail.com>
2017-05-11 18:30:08 +0200

    Initialize Content Provider and DBHelper in it.

---
+++ app/src/main/java/com/gmail/lusersks/notes/provider/NotesProvider.java
@@ -0,0 +1,69 @@
+package com.gmail.lusersks.notes.provider;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+
+public class NotesProvider extends ContentProvider {
+
+    public static final int VERSION_DB = 1;
+    public static final String NOTES_DB = "notesDB";
+    private DBHelper db;
+
+    @Override
+    public boolean onCreate() {
+        db = new DBHelper(getContext(), NOTES_DB, null, VERSION_DB);
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    private class DBHelper extends SQLiteOpenHelper {
+        public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+            super(context, name, factory, version);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+    }
+}
